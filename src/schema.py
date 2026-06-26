4c49a9753bb184eca992387103ae60bd5520c5d7
Sergey Kuznetsov <srgykuz@gmail.com>
2026-06-26 11:20:14 +0300

    schema: fix incorrect usage of optional fields

--- src/schema.py
+++ src/schema.py
@@ -15,8 +15,8 @@ class BaseModelJSON(BaseModel):
 
 
 class User(BaseModelJSON):
-    first_name: Optional[str]
-    last_name: Optional[str]
+    first_name: str
+    last_name: str = Field(default="")
 
 
 class Persona(BaseModelJSON):
